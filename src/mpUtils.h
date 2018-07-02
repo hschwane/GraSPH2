01496312c793d665d39f0791b9bc0ea46b63fb15
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-07-02 17:46:40 +0200

    add catch all header mpUtils for inclusion by executables

--- src/mpUtils.h
+++ src/mpUtils.h
@@ -11,7 +11,37 @@
 #ifndef MPUTILS_MPUTILS_H
 #define MPUTILS_MPUTILS_H
 
+// general stuff
+#include "stringUtils.h"
+#include "timeUtils.h"
 
+// include configuration util
+#include "Cfg/CfgFile.h"
+
+// include the logger
+#include "Log/ConsoleSink.h"
+#include "Log/FileSink.h"
+#include "Log/Log.h"
+
+#ifdef __linux__
+    #include "Log/SyslogSink.h"
+#endif
+
+// include timer
+#include "Timer/AsyncTimer.h"
+#include "Timer/DeltaTimer.h"
+#include "Timer/Stopwatch.h"
+#include "Timer/Timer.h"
+
+// include graphics
+#ifdef USE_OPENGL
+    #include "Graphics/Graphics.h"
+#endif
+
+// include cuda
+#ifdef USE_CUDA
+    #include "Cuda/cudaUtils.h"
+#endif
 
 
 #endif //MPUTILS_MPUTILS_H
