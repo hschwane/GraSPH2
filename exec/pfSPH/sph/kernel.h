7c498f12ff37dcbb56c93fac38bf36dbe303d14a
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-09-05 12:13:15 +0200

    add namespace kernel for sph kernel functions

--- exec/pfSPH/sph/kernel.h
+++ exec/pfSPH/sph/kernel.h
@@ -28,55 +28,63 @@
 #endif
 //--------------------
 
+namespace kernel {
+
 namespace detail {
 
-    /**
-     * @brief calculates the dimension dependent prefactor of the spline functions
-     * @tparam dimension Dim::one, Dim::two, or Dim::three
-     * @param h the smoothing length
-     * @return the dimension dependent prefactor of the spline functions
-     */
-    template<Dim dimension>
-    CUDAHOSTDEV f1_t splinePrefactor(f1_t h) {return 0;}
+/**
+ * @brief calculates the dimension dependent prefactor of the spline functions
+ * @tparam dimension Dim::one, Dim::two, or Dim::three
+ * @param h the smoothing length
+ * @return the dimension dependent prefactor of the spline functions
+ */
+template<Dim dimension>
+CUDAHOSTDEV f1_t splinePrefactor(f1_t h) { return 0; }
 
-    template<> CUDAHOSTDEV f1_t splinePrefactor<Dim::one>(f1_t h)
-    {
-        return 4.0 / (3.0*h);
-    }
+template<>
+CUDAHOSTDEV f1_t splinePrefactor<Dim::one>(f1_t h)
+{
+    return 4.0 / (3.0 * h);
+}
 
-    template<> CUDAHOSTDEV f1_t splinePrefactor<Dim::two>(f1_t h)
-    {
-        return 40.0 / (7.0*M_PI*h*h);
-    }
+template<>
+CUDAHOSTDEV f1_t splinePrefactor<Dim::two>(f1_t h)
+{
+    return 40.0 / (7.0 * M_PI * h * h);
+}
 
-    template<> CUDAHOSTDEV f1_t splinePrefactor<Dim::three>(f1_t h)
-    {
-        return  8.0 / (M_PI*h*h*h);
-    }
+template<>
+CUDAHOSTDEV f1_t splinePrefactor<Dim::three>(f1_t h)
+{
+    return 8.0 / (M_PI * h * h * h);
+}
 
-    /**
-    * @brief calculates the dimension dependent prefactor of the first derivatives spline functions
-    * @tparam dimension Dim::one, Dim::two, or Dim::three
-    * @param h the smoothing length
-    * @return the dimension dependent prefactor of the spline functions
-    */
-    template<Dim dimension>
-    CUDAHOSTDEV f1_t dsplinePrefactor(f1_t h) {return 0;}
+/**
+* @brief calculates the dimension dependent prefactor of the first derivatives spline functions
+* @tparam dimension Dim::one, Dim::two, or Dim::three
+* @param h the smoothing length
+* @return the dimension dependent prefactor of the spline functions
+*/
+template<Dim dimension>
+CUDAHOSTDEV f1_t dsplinePrefactor(f1_t h) { return 0; }
 
-    template<> CUDAHOSTDEV f1_t dsplinePrefactor<Dim::one>(f1_t h)
-    {
-        return 24.0 / (3.0*h*h);
-    }
+template<>
+CUDAHOSTDEV f1_t dsplinePrefactor<Dim::one>(f1_t h)
+{
+    return 24.0 / (3.0 * h * h);
+}
 
-    template<> CUDAHOSTDEV f1_t dsplinePrefactor<Dim::two>(f1_t h)
-    {
-        return 240.0 / (7.0*M_PI*h*h*h);
-    }
+template<>
+CUDAHOSTDEV f1_t dsplinePrefactor<Dim::two>(f1_t h)
+{
+    return 240.0 / (7.0 * M_PI * h * h * h);
+}
 
-    template<> CUDAHOSTDEV f1_t dsplinePrefactor<Dim::three>(f1_t h)
-    {
-        return 48.0 / (M_PI*h*h*h*h);
-    }
+template<>
+CUDAHOSTDEV f1_t dsplinePrefactor<Dim::three>(f1_t h)
+{
+    return 48.0 / (M_PI * h * h * h * h);
+}
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -92,11 +100,11 @@ namespace detail {
 template<Dim dimension>
 CUDAHOSTDEV f1_t Wspline(f1_t r, f1_t h)
 {
-    f1_t q = r/h;
+    f1_t q = r / h;
     return detail::splinePrefactor<dimension>(h)
-            * (q<1.0) ? ((q<0.5) ? (6*q*q*q - 6*q*q +1)
-                                 : 2*(1-q)*(1-q)*(1-q))
-                                 : f1_t(0.0);
+           * (q < 1.0) ? ((q < 0.5) ? (6 * q * q * q - 6 * q * q + 1)
+                                    : 2 * (1 - q) * (1 - q) * (1 - q))
+                       : f1_t(0.0);
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -112,11 +120,11 @@ CUDAHOSTDEV f1_t Wspline(f1_t r, f1_t h)
 template<Dim dimension>
 CUDAHOSTDEV f1_t dWspline(f1_t r, f1_t h)
 {
-    f1_t q = r/h;
+    f1_t q = r / h;
     return detail::dsplinePrefactor<dimension>(h)
-           * (q<1.0) ? ((q<0.5) ? (3*q*q - 2*q +1)
-                                : -1*(1-q)*(1-q))
-                                : f1_t(0.0);
+           * (q < 1.0) ? ((q < 0.5) ? (3 * q * q - 2 * q + 1)
+                                    : -1 * (1 - q) * (1 - q))
+                       : f1_t(0.0);
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -129,7 +137,7 @@ CUDAHOSTDEV f1_t dWspline(f1_t r, f1_t h)
  */
 CUDAHOSTDEV inline f1_t Wpoly6Factor(f1_t h)
 {
-    return (315.0 / (64.0* M_PI * pow(h,9.0)));
+    return (315.0 / (64.0 * M_PI * pow(h, 9.0)));
 }
 
 /**
@@ -140,9 +148,9 @@ CUDAHOSTDEV inline f1_t Wpoly6Factor(f1_t h)
  */
 CUDAHOSTDEV inline f1_t Wpoly6(f1_t r2, f1_t h)
 {
-    const f1_t h2 = h*h;
-    const f1_t hmr = h2-r2;
-    return (r2 < h2) ? Wpoly6Factor(h) *hmr*hmr*hmr :0.0;
+    const f1_t h2 = h * h;
+    const f1_t hmr = h2 - r2;
+    return (r2 < h2) ? Wpoly6Factor(h) * hmr * hmr * hmr : 0.0;
 }
 
 /**
@@ -154,8 +162,8 @@ CUDAHOSTDEV inline f1_t Wpoly6(f1_t r2, f1_t h)
  */
 CUDAHOSTDEV inline f1_t Wpoly6(f1_t r2, f1_t h2, f1_t factor)
 {
-    const f1_t hmr = h2-r2;
-    return (r2 < h2) ? factor *hmr*hmr*hmr :0.0;
+    const f1_t hmr = h2 - r2;
+    return (r2 < h2) ? factor * hmr * hmr * hmr : 0.0;
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -168,7 +176,7 @@ CUDAHOSTDEV inline f1_t Wpoly6(f1_t r2, f1_t h2, f1_t factor)
  */
 CUDAHOSTDEV inline f1_t dWpoly6Factor(f1_t h)
 {
-    return (-945.0 / (32.0* M_PI * pow(h,9.0)));
+    return (-945.0 / (32.0 * M_PI * pow(h, 9.0)));
 }
 
 /**
@@ -179,9 +187,9 @@ CUDAHOSTDEV inline f1_t dWpoly6Factor(f1_t h)
  */
 CUDAHOSTDEV inline f1_t dWpoly6(f1_t r2, f1_t h)
 {
-    const f1_t h2 = h*h;
-    const f1_t hmr = h2-r2;
-    return (r2 < h2) ? dWpoly6Factor(h) *hmr*hmr  :0.0;
+    const f1_t h2 = h * h;
+    const f1_t hmr = h2 - r2;
+    return (r2 < h2) ? dWpoly6Factor(h) * hmr * hmr : 0.0;
 }
 
 /**
@@ -193,8 +201,8 @@ CUDAHOSTDEV inline f1_t dWpoly6(f1_t r2, f1_t h)
  */
 CUDAHOSTDEV inline f1_t dWpoly6(f1_t r2, f1_t h2, f1_t factor)
 {
-    const f1_t hmr = h2-r2;
-    return (r2 < h2) ? factor *hmr*hmr  :0.0;
+    const f1_t hmr = h2 - r2;
+    return (r2 < h2) ? factor * hmr * hmr : 0.0;
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -207,7 +215,7 @@ CUDAHOSTDEV inline f1_t dWpoly6(f1_t r2, f1_t h2, f1_t factor)
  */
 CUDAHOSTDEV inline f1_t dWspikyFactor(f1_t h)
 {
-    return (-45.0 / (M_PI * pow(h,6.0)));
+    return (-45.0 / (M_PI * pow(h, 6.0)));
 }
 
 /**
@@ -218,8 +226,8 @@ CUDAHOSTDEV inline f1_t dWspikyFactor(f1_t h)
  */
 CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h)
 {
-    float hmr = h-r;
-    return (r < h) ? dWspikyFactor(h) * hmr*hmr :0.0;
+    float hmr = h - r;
+    return (r < h) ? dWspikyFactor(h) * hmr * hmr : 0.0;
 }
 
 /**
@@ -231,8 +239,10 @@ CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h)
  */
 CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h, f1_t factor)
 {
-    float hmr = h-r;
-    return (r < h) ? factor * hmr*hmr :0.0;
+    float hmr = h - r;
+    return (r < h) ? factor * hmr * hmr : 0.0;
+}
+
 }
 
 #endif //MPUTILS_KERNEL_H
