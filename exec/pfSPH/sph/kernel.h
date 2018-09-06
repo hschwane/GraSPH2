17b37560a4b22f3f9869ee59aaee47e314a0c1a0
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-09-06 11:49:27 +0200

    change and fix kernels

--- exec/pfSPH/sph/kernel.h
+++ exec/pfSPH/sph/kernel.h
@@ -39,7 +39,7 @@ namespace detail {
  * @return the dimension dependent prefactor of the spline functions
  */
 template<Dim dimension>
-CUDAHOSTDEV f1_t splinePrefactor(f1_t h) { return 0; }
+CUDAHOSTDEV f1_t splinePrefactor(f1_t h) {throw std::logic_error("illegal dimension for this kernel");}
 
 template<>
 CUDAHOSTDEV f1_t splinePrefactor<Dim::one>(f1_t h)
@@ -66,7 +66,7 @@ CUDAHOSTDEV f1_t splinePrefactor<Dim::three>(f1_t h)
 * @return the dimension dependent prefactor of the spline functions
 */
 template<Dim dimension>
-CUDAHOSTDEV f1_t dsplinePrefactor(f1_t h) { return 0; }
+CUDAHOSTDEV f1_t dsplinePrefactor(f1_t h) {} // {throw std::logic_error("illegal dimension for this kernel");}
 
 template<>
 CUDAHOSTDEV f1_t dsplinePrefactor<Dim::one>(f1_t h)
@@ -85,10 +85,47 @@ CUDAHOSTDEV f1_t dsplinePrefactor<Dim::three>(f1_t h)
 {
     return 48.0 / (M_PI * h * h * h * h);
 }
+
+/**
+* @brief calculates the dimension dependent prefactor of the first derivatives spiky kernel
+* @tparam dimension Dim::one, Dim::two, or Dim::three
+* @param h the smoothing length
+* @return the dimension dependent prefactor of the spiky kernel
+*/
+template<Dim dimension>
+CUDAHOSTDEV f1_t dspikyPrefactor(f1_t h) {}// {throw std::logic_error("illegal dimension for this kernel");}
+
+template<>
+CUDAHOSTDEV f1_t dspikyPrefactor<Dim::two>(f1_t h)
+{
+    return (-30.0 / (M_PI * h*h*h));
+}
+
+template<>
+CUDAHOSTDEV f1_t dspikyPrefactor<Dim::three>(f1_t h)
+{
+    return -45.0 / (M_PI * h*h*h*h);
+}
+
 }
 
 // -----------------------------------------------------------------------------------------------------
 // B-Spline
+
+/**
+ * @brief calculates the b-spline kernel function Monaghan & Lattanzio (1985)
+ * @param factor dimension dependend precomputed prefactor (use detail::splinePrefactor)
+ * @param r the distance from the sample point r
+ * @param h the smoothing length
+ * @return value of the spline kernel function
+ */
+CUDAHOSTDEV f1_t Wspline(f1_t r, f1_t h, f1_t factor)
+{
+    f1_t q = r / h;
+    return factor *( (q <= 1.0) ? ((q < 0.5) ? (6 * q * q * q - 6 * q * q + 1)
+                                     : 2 * (1 - q) * (1 - q) * (1 - q))
+                        : f1_t(0.0));
+}
 
 /**
  * @brief calculates the b-spline kernel function Monaghan & Lattanzio (1985)
@@ -98,17 +135,28 @@ CUDAHOSTDEV f1_t dsplinePrefactor<Dim::three>(f1_t h)
  * @return value of the spline kernel function
  */
 template<Dim dimension>
-CUDAHOSTDEV f1_t Wspline(f1_t r, f1_t h)
+CUDAHOSTDEV inline f1_t Wspline(f1_t r, f1_t h)
 {
-    f1_t q = r / h;
-    return detail::splinePrefactor<dimension>(h)
-           * (q < 1.0) ? ((q < 0.5) ? (6 * q * q * q - 6 * q * q + 1)
-                                    : 2 * (1 - q) * (1 - q) * (1 - q))
-                       : f1_t(0.0);
+    return Wspline(r,h,detail::splinePrefactor<dimension>(h));
 }
 
 // -----------------------------------------------------------------------------------------------------
 // B-Spline derivative
+
+/**
+ * @brief calculates the first derivative of the b-spline kernel function Monaghan & Lattanzio (1985)
+ * @param factor dimension dependend precomputed prefactor (use detail::dsplinePrefactor)
+ * @param r the distance from the sample point r
+ * @param h the smoothing length
+ * @return value of the spline function derivative
+ */
+CUDAHOSTDEV inline f1_t dWspline(f1_t r, f1_t h, f1_t factor)
+{
+    f1_t q = r / h;
+    return factor *( (q <= 1.0) ? ((q < 0.5) ? (3 * q * q - 2 * q)
+                                     : -1 * (1 - q) * (1 - q))
+                        : f1_t(0.0));
+}
 
 /**
  * @brief calculates the first derivative of the b-spline kernel function Monaghan & Lattanzio (1985)
@@ -120,11 +168,35 @@ CUDAHOSTDEV f1_t Wspline(f1_t r, f1_t h)
 template<Dim dimension>
 CUDAHOSTDEV f1_t dWspline(f1_t r, f1_t h)
 {
-    f1_t q = r / h;
-    return detail::dsplinePrefactor<dimension>(h)
-           * (q < 1.0) ? ((q < 0.5) ? (3 * q * q - 2 * q + 1)
-                                    : -1 * (1 - q) * (1 - q))
-                       : f1_t(0.0);
+    return dWspline(r,h,detail::dsplinePrefactor<dimension>(h));
+}
+
+// -----------------------------------------------------------------------------------------------------
+// spiky derivative
+
+/**
+ * @brief calculate the first derivative of the spiky kernel function Desbrun 1996
+ * @param r2 the square of the distance to the sample point
+ * @param h the smoothing length
+ * @param factor the precomputed prefactor (use detail::dspikyPrefactor)
+ * @return the value of the poly6 derivative
+ */
+CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h, f1_t factor)
+{
+    float q = r/h;
+    return (q < 1) ? factor * (1.0-q) * (1.0-q) : 0.0;
+}
+
+/**
+ * @brief calculate the first derivative of the spiky kernel function Desbrun 1996
+ * @param r2 the square of the distance to the sample point
+ * @param h the smoothing length
+ * @return the value of the poly6 derivative
+ */
+template<Dim dimension>
+CUDAHOSTDEV f1_t dWspiky(f1_t r, f1_t h)
+{
+    return dWspiky(r,h,detail::dspikyPrefactor<dimension>(h));
 }
 
 // -----------------------------------------------------------------------------------------------------
@@ -205,43 +277,6 @@ CUDAHOSTDEV inline f1_t dWpoly6(f1_t r2, f1_t h2, f1_t factor)
     return (r2 < h2) ? factor * hmr * hmr : 0.0;
 }
 
-// -----------------------------------------------------------------------------------------------------
-// spiky derivative
-
-/**
- * @brief calculate the prefactor of the derivative of the poly6 kernel which can be saved for performance reasons
- * @param h the smoothing length
- * @return the prefactor of the poly6 kernel
- */
-CUDAHOSTDEV inline f1_t dWspikyFactor(f1_t h)
-{
-    return (-45.0 / (M_PI * pow(h, 6.0)));
-}
-
-/**
- * @brief calculate the first derivative of the spiky kernel function Desbrun 1996
- * @param r2 the square of the distance to the sample point
- * @param h the smoothing length
- * @return the value of the poly6 derivative
- */
-CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h)
-{
-    float hmr = h - r;
-    return (r < h) ? dWspikyFactor(h) * hmr * hmr : 0.0;
-}
-
-/**
- * @brief calculate the first derivative of the spiky kernel function Desbrun 1996
- * @param r2 the square of the distance to the sample point
- * @param h the smoothing length
- * @param factor the precomputed prefactor
- * @return the value of the poly6 derivative
- */
-CUDAHOSTDEV inline f1_t dWspiky(f1_t r, f1_t h, f1_t factor)
-{
-    float hmr = h - r;
-    return (r < h) ? factor * hmr * hmr : 0.0;
-}
 
 }
 
