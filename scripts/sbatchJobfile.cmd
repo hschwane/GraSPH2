62596872b6c2e5da781a9d1b05c97ddbc7f0bc81
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-12-12 03:12:36 +0100

    change job name in slurm script

--- scripts/sbatchJobfile.cmd
+++ scripts/sbatchJobfile.cmd
@@ -5,18 +5,18 @@
 # Initial working directory:
 #SBATCH -D ./
 #
-#SBATCH -J test_slurm
+#SBATCH -J GraSPH2
 # Queue:
-#SBATCH --partition=gpu1        # If using only 1 GPU of a shared node
+#SBATCH --partition=gpu1
 # Node feature:
 #SBATCH --constraint="gpu"
 # Specify number of GPUs to use:
-#SBATCH --gres=gpu:1            # If using only 1 GPU of a shared node
-#SBATCH --mem=61000             # Memory is necessary if using only 1 GPU
+#SBATCH --gres=gpu:1
+#SBATCH --mem=61000
 #
 # Number of nodes and MPI tasks per node:
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1      # If using both GPUs of a node
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #
 #SBATCH --mail-type=END
@@ -25,10 +25,10 @@
 # wall clock limit:
 #SBATCH --time=24:00:00
 
-module load cuda 
+module load cuda
 
 export OMP_NUM_THREADS=4
-export OMP_PLACES=cores 
+export OMP_PLACES=cores
 
 # Run the program:
 srun ../build/GraSPH2 > prog.out
